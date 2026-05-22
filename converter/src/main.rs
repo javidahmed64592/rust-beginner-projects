35175924ccb8241358121b36486018a9db0c5e44
Javid Ahmed <javidahmed@icloud.com>
2026-05-22 23:08:26 +0100

    Refactor argument handling in main function for clearer usage instructions

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -6,10 +6,13 @@ use crate::units::Temperature;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let num_args = args.len();
-    let v = args[num_args - 3].parse::<f64>().unwrap();
-    let u1: TempUnit = args[num_args - 2].parse().expect("Invalid unit");
-    let u2: TempUnit = args[num_args - 1].parse().expect("Invalid unit");
+    if args.len() != 4 {
+        eprintln!("Usage: converter <value> <from_unit> <to_unit>");
+        std::process::exit(1);
+    }
+    let v = args[1].parse::<f64>().unwrap();
+    let u1: TempUnit = args[2].parse().expect("Invalid unit");
+    let u2: TempUnit = args[3].parse().expect("Invalid unit");
 
     let temp = Temperature { value: v, unit: u1 };
     let result = temp.convert_to(u2);
