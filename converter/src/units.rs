8322d8aaa0622a8fc2b615647050d141f820ecd7
Javid Ahmed <javidahmed@icloud.com>
2026-05-22 01:16:04 +0100

    Implement TempUnit conversion from string and add corresponding tests

--- converter/src/units.rs
+++ converter/src/units.rs
@@ -1,9 +1,24 @@
 use approx::assert_abs_diff_eq;
+use std::str::FromStr;
 
+#[derive(Debug, PartialEq)]
 enum TempUnit {
     Celsius,
     Fahrenheit,
     Kelvin,
+}
+
+impl std::str::FromStr for TempUnit {
+    type Err = String;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        match value.to_lowercase().as_str() {
+            "c" => Ok(TempUnit::Celsius),
+            "f" => Ok(TempUnit::Fahrenheit),
+            "k" => Ok(TempUnit::Kelvin),
+            _ => Err(format!("Unknown unit: '{}'", value)),
+        }
+    }
 }
 
 impl TempUnit {
@@ -25,7 +40,36 @@ impl TempUnit {
 }
 
 #[cfg(test)]
-mod convert_tests {
+mod temp_unit_from_string_tests {
+    use super::*;
+
+    #[test]
+    fn test_celsius() {
+        let t = TempUnit::from_str("c");
+        assert_eq!(t.unwrap(), TempUnit::Celsius);
+    }
+
+    #[test]
+    fn test_fahrenheit() {
+        let t = TempUnit::from_str("f");
+        assert_eq!(t.unwrap(), TempUnit::Fahrenheit);
+    }
+
+    #[test]
+    fn test_kelvin() {
+        let t = TempUnit::from_str("k");
+        assert_eq!(t.unwrap(), TempUnit::Kelvin);
+    }
+
+    #[test]
+    fn test_unknown_unit() {
+        let t = TempUnit::from_str("a");
+        assert!(t.is_err());
+    }
+}
+
+#[cfg(test)]
+mod temp_unit_convert_tests {
     use super::*;
 
     #[test]
